7bea18bf42ad73aec917d12a807f416f26e0eb6f
Tomas Moyano <tomimoyanook@gmail.com>
2024-09-30 17:19:30 -0300

    responsive swipe

--- app.js
+++ app.js
@@ -5,13 +5,18 @@ var swiper = new Swiper(".mySwiper", {
     slidesPerView: "auto",
     loop:true,
     coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
+      rotate: -10,
+      stretch: 50,
+      depth: 350,
       modifier: 1,
       slideShadows: true,
     },
     pagination: {
       el: ".swiper-pagination",
     },
+    on:{
+      click(event){
+        swiper.slideTo(this.clickedIndex);
+      }
+    }
   });
